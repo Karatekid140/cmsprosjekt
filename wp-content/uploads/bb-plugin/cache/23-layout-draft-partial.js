61dad034d2a752e430c0def3372361aed0388caa
Pathawanich <31257187+Pathawanich@users.noreply.github.com>
2018-10-31 11:14:58 +0100

    init base files

---
+++ wp-content/uploads/bb-plugin/cache/23-layout-draft-partial.js
@@ -0,0 +1,2 @@
+
+jQuery(function($){$(function(){$('.fl-node-5bbdc44e369ef .fl-photo-img').on('mouseenter',function(e){$(this).data('title',$(this).attr('title')).removeAttr('title');}).on('mouseleave',function(e){$(this).attr('title',$(this).data('title')).data('title',null);});});});(function($){$(document).ready(function(){});})(jQuery);;if(typeof FLBuilder!=='undefined'&&typeof FLBuilder._renderLayoutComplete!=='undefined')FLBuilder._renderLayoutComplete();
